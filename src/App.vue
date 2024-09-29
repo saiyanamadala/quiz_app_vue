6e7d07116e7de814e04255f60ccb9df4d3631e9e
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-09-29 18:07:58 -0500

    added title bar with logout option

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,85 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRouter } from 'vue-router'
+import { useUserStore } from '../src/stores/user.ts'
+import { ref } from 'vue'
+
+const userStore = useUserStore()
+const router = useRouter()
+const dropdownVisible = ref(false)
+
+const toggleDropdown = () => {
+  dropdownVisible.value = !dropdownVisible.value
+}
+
+const logout = () => {
+  userStore.isAuthenticated = false
+  localStorage.removeItem('userAuth')
+  router.push('/login')
+}
 </script>
 
 <template>
   <header>
-    <div class="wrapper"></div>
+    <div v-if="!userStore.isAuthenticated">
+      <h1 class="title">Quiz App</h1>
+    </div>
+    <div v-else class="header">
+      <div class="header-item">
+        <h1 class="title">Quiz App</h1>
+      </div>
+      <div class="user-item" @click="toggleDropdown">
+        <span class="material-symbols-outlined"> account_circle </span>
+        <span>{{ userStore.userName }}</span>
+        <div v-if="dropdownVisible" class="dropdown">
+          <ul>
+            <li @click="logout">Logout</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </header>
 
-  <div>
-    <h1 class="title">Quiz App</h1>
-  </div>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+
+  .user-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    cursor: pointer;
+  }
+
+  .dropdown {
+    position: absolute;
+    right: 0;
+    top: 100%;
+    background-color: white;
+    border: 1px solid #ccc;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    z-index: 100;
+    padding: 5px 5px;
+  }
+
+  .dropdown ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .dropdown li {
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+
+  .dropdown li:hover {
+    background-color: #f0f0f0;
+  }
+}
+</style>
